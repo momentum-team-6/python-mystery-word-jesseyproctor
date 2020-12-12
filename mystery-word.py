5cf2493e3156e66137f73c6f2a3a283f2bc6e2e0
jesseyproctor <jss.proctor@gmail.com>
2020-12-11 22:46:12 -0500

    debug levels 2

--- mystery-word.py
+++ mystery-word.py
@@ -6,34 +6,34 @@ word_list = read_file.split()
 # print(type(word_list))
 
 #create easy, normal and hard word_lists:
-easy_list = []
-normal_list = []
-hard_list = []
 #seperate all returned words in word_list in to three catogories based on length
-def create_easy_word_list(list, new_list):
+def create_easy_word_list(list):
+    new_list = []
     for word in list:
         if len(word) >= 4 and len(word) <= 5:
             new_list.append(word)
         # print(new_list)
         return(new_list)
 
-def create_normal_word_list(list, new_list):
+def create_normal_word_list(list):
+    new_list = []
     for word in list:
         if len(word) >= 6 and len(word) <= 8:
             new_list.append(word)
         # print(new_list)
         return(new_list)
 
-def create_hard_word_list(list, new_list):
+def create_hard_word_list(list):
+    new_list = []
     for word in list:
         if len(word) > 8:
             new_list.append(word)
         # print(new_list)
         return(new_list)
 
-create_easy_word_list(word_list, easy_list)
-create_normal_word_list(word_list, normal_list)
-create_hard_word_list(word_list, hard_list)
+easy_list = create_easy_word_list(word_list)
+normal_list = create_normal_word_list(word_list)
+hard_list = create_hard_word_list(word_list)
 
 #Player chooses difficulty level and computer selects word:
 import random
@@ -61,12 +61,22 @@ else:
 # allowed_guesses = 8
 # used_guesses = 0
 # game_over = False
+# word = 
+# word_display = []
 
 # while game_over == False:
 #     print(")
-#     guess = int(input())
+#     guess = input()
 #     used_guesses +=1
-#insert all code from code shell from thursday night for game
-   
+      
+#     if guess == computer_num:
+#         print('You won!')
+#     game_over = True
+#     else:
+#         print("Your guess was incorrect. You have", allowed_guesses - used_guesses, "remaining guesses")
+#             if used_guesses == allowed_guesses:
+#                 print("Game Over")
+#                 game_over = True
+
 
 
