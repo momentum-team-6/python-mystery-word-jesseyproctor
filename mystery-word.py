f5ee6e39f1aeb768f7ffc81c26cd05d67cbef52f
jesseyproctor <jss.proctor@gmail.com>
2020-12-12 20:05:50 -0500

    add validation to levels

--- mystery-word.py
+++ mystery-word.py
@@ -12,7 +12,7 @@ def create_easy_word_list(list):
     for word in list:
         if len(word) >= 4 and len(word) <= 5:
             new_list.append(word)
-        # print(new_list)
+    # print(new_list)
     return(new_list)
 
 def create_normal_word_list(list):
@@ -20,7 +20,7 @@ def create_normal_word_list(list):
     for word in list:
         if len(word) >= 6 and len(word) <= 8:
             new_list.append(word)
-        # print(new_list)
+    # print(new_list)
     return(new_list)
 
 def create_hard_word_list(list):
@@ -28,33 +28,43 @@ def create_hard_word_list(list):
     for word in list:
         if len(word) > 8:
             new_list.append(word)
-        # print(new_list)
+    # print(new_list)
     return(new_list)
 
 easy_list = create_easy_word_list(word_list)
-# print(easy_list) #this is where the code is broken
 normal_list = create_normal_word_list(word_list)
 hard_list = create_hard_word_list(word_list)
 
 #Player chooses difficulty level and computer selects word:
 import random
-level = input('Please select game mode. Enter "easy", "normal", or "hard": ')
+ 
+while True:
+    level = input('Please select game mode. Enter "easy", "normal", or "hard": ')
+    if level != "easy" and level != "Easy" and level != "normal" and level != "Normal" and level != "hard" and level != "Hard": 
+        print(f'Error, "{level}" is not a game mode.')
+        continue
+    else:
+        break
+
 if level == 'easy' or level == 'Easy':
     easy_word = random.choice(easy_list)
-    print(easy_word)
+    # print(easy_word)
+    returned_word = easy_word
 elif level == 'normal' or level == 'Normal':
     normal_word = random.choice(normal_list)
-    print(normal_word)
+    # print(normal_word)
+    returned_word = normal_word
 elif level == 'hard' or level == 'Hard':
     hard_word = random.choice(hard_list)
-    print(hard_word)
+    # print(hard_word)
+    returned_word = hard_word
 else:
     print(f'Error, "{level}" is not a game mode.')
+#This else statement is unnecesary except to close out loop.  How do I get rid of it or what do I put in it's place?
+#also, I was orginignally trying to combine if statements and while loop because I thought the the level variable I defined 
+#in loop would dissapear after loop was used.  Why does this work?
 
-# if level != "easy" and level != "Easy" and level != "normal" and level != "Normal" and level != "hard" and level != "Hard": 
-#     print(level)
-# else:
-#     print(f'Error, "{level}" is not a game mode.')
+print(returned_word)
 
 
 
@@ -63,7 +73,7 @@ else:
 # used_guesses = 0
 # game_over = False
 # word = 
-# word_display = []
+# word_display = ['_' * len(returned_word)]
 
 # while game_over == False:
 #     print(")
